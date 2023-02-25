86c2d78bb938afff05e9a1d3c6138aed46257099
Gerard <bastien.gerard@epitech.eu>
2023-02-25 19:57:37 +0100

    UPDATE: C4001 and C4011

--- src/C4001.cpp
+++ src/C4001.cpp
@@ -10,12 +10,12 @@
 nts::Tristate nts::C4001::compute(std::size_t pin)
 {
     if (pin == 3)
-        return (!getLink(1)->compute()) | (!getLink(2)->compute());
+        return (!getLink(1)->compute() | !getLink(2)->compute());
     if (pin == 4)
-        return (!getLink(5)->compute()) | (!getLink(6)->compute());
+        return (!getLink(5)->compute() | !getLink(6)->compute());
     if (pin == 10)
-        return (!getLink(8)->compute()) | (!getLink(9)->compute());
+        return (!getLink(8)->compute() | !getLink(9)->compute());
     if (pin == 11)
-        return (!getLink(12)->compute()) | (!getLink(13)->compute());
+        return (!getLink(12)->compute() | !getLink(13)->compute());
     return nts::UNDEFINED;
 }
